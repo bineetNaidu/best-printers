1271e5cada5a46fb506af676b294f33cd8d9d29c
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-05 22:53:42 +0530

    Updated Header Component

--- components/Headers.tsx
+++ components/Headers.tsx
@@ -4,6 +4,18 @@ import styles from './Header.module.css';
 import Link from 'next/link';
 import { projectAuth } from '../firebase';
 import { useRouter } from 'next/router';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { useState } from 'react';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import InfoIcon from '@material-ui/icons/Info';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 
 type User = {
   username: string;
@@ -20,27 +32,81 @@ const Headers: React.FC<Props> = ({ user }) => {
     projectAuth.signOut();
     router.push('/');
   };
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className={styles.header}>
-      <div>
-        <MenuIcon />
+    <>
+      <div className={styles.header}>
+        <div>
+          <IconButton onClick={handleDrawerOpen}>
+            <MenuIcon />
+          </IconButton>
+        </div>
+        <p className={styles.header__brand}>
+          <Link href="/">The BestPrinters.in</Link>
+        </p>
+        <div>
+          {user ? (
+            <>
+              <Button>{user.username}</Button>
+              <Button onClick={handleSignOut}>Logout</Button>
+            </>
+          ) : (
+            <Button>
+              <Link href="/login">Login</Link>
+            </Button>
+          )}
+        </div>
       </div>
-      <p className={styles.header__brand}>
-        <Link href="/">The BestPrinters.in</Link>
-      </p>
-      <div>
-        {user ? (
-          <>
-            <Button>{user.username}</Button>
-            <Button onClick={handleSignOut}>Logout</Button>
-          </>
-        ) : (
-          <Button>
-            <Link href="/login">Login</Link>
-          </Button>
-        )}
-      </div>
-    </div>
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={open}
+        className={styles.header__drawer}
+      >
+        <div>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronRightIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button>
+            <Link href="/about">
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary="About" />
+            </Link>
+          </ListItem>
+          <ListItem button>
+            <Link href="/contact">
+              <ListItemIcon>
+                <ContactSupportIcon />
+              </ListItemIcon>
+              <ListItemText primary="Contact Us" />
+            </Link>
+          </ListItem>
+          <ListItem button>
+            <Link href="/admin">
+              <ListItemIcon>
+                <SupervisorAccountIcon />
+              </ListItemIcon>
+              <ListItemText primary="Admin" />
+            </Link>
+          </ListItem>
+        </List>
+        <Divider />
+      </Drawer>
+    </>
   );
 };
 
