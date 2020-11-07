7065b54fe506009c8e62c36f3aede73cdb697d2d
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-07 12:41:38 +0530

    Updated Footer w/ styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,57 +9,14 @@ import Typography from '@material-ui/core/Typography';
 
 const Footer: React.FC = () => {
   return (
-    <>
-      <div className={styles.footer}>
-        <div className={styles.footer__left}>
-          <img src="/joinus.png" className={styles.footer__img} />
-          <h1>Follow</h1>
-          <div className={styles.footer__followLinks}>
-            <IconButton color="inherit">
-              <FacebookIcon />
-            </IconButton>
-            <IconButton color="inherit">
-              <InstagramIcon />
-            </IconButton>
-            <IconButton color="inherit">
-              <TwitterIcon />
-            </IconButton>
-          </div>
-        </div>
-        <div className={styles.footer__right}>
-          <form>
-            <TextField
-              color="primary"
-              margin="normal"
-              label="Name"
-              variant="outlined"
-            />
-            <TextField
-              color="primary"
-              margin="normal"
-              label="Email"
-              variant="outlined"
-            />
-            <TextField
-              color="primary"
-              margin="normal"
-              label="Message*"
-              multiline
-              variant="outlined"
-            />
-            <Button variant="outlined" color="inherit">
-              Send
-            </Button>
-          </form>
-        </div>
-      </div>
+    <div className={styles.footer}>
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
         {new Date().getFullYear()}
         {` `}
         BestPrinters.in
       </Typography>
-    </>
+    </div>
   );
 };
 
