3ca9406d8daf0769ff1032bb4a011f39c41ee87a
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-10 13:29:43 +0530

    firebase prod intregations

--- firebase.ts
+++ firebase.ts
@@ -1,19 +1,25 @@
 import firebase from 'firebase';
 import 'firebase/auth';
+// import 'firebase/analytics';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MSG_SENDING_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MESUASEMENT_ID,
+  apiKey: 'AIzaSyBeAlIGYEfRRAk5J5v0cfu7dGgfBLBNC48',
+  authDomain: 'the-best-printers-in-prod.firebaseapp.com',
+  databaseURL: 'https://the-best-printers-in-prod.firebaseio.com',
+  projectId: 'the-best-printers-in-prod',
+  storageBucket: 'the-best-printers-in-prod.appspot.com',
+  messagingSenderId: '129400031893',
+  appId: '1:129400031893:web:76e8f80306a4aa2098e99c',
+  measurementId: 'G-46SMF8V235',
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
 export const projectAuth = firebase.auth();
 export const projectAuthProvider = new firebase.auth.GoogleAuthProvider();
+// export const projectAnalytics = firebase.analytics();
